20d29c316f1e09e092434975d6d89aae17144229
Jason Runzer <jason.runzer@uoit.net>
2015-11-23 21:02:10 +0000

    update

--- Pi/GUI.py
+++ Pi/GUI.py
@@ -1,13 +1,57 @@
 #!/usr/bin/python
-import Tkinter
-import tkMessageBox
 
-top = Tkinter.Tk()
+import sys
+from Tkinter import *
 
-def helloCallBack():
-   tkMessageBox.showinfo( "Hello Python", "Hello World")
+class GUI (object):
+   def updateSong(self,message):
+      Csong.set(message)
+  
+   
+   def __init__(self,reset,pausePlay,skip):
+      self.reset = reset
+      self.pausePlay = pausePlay
+      self.skip = skip
+      
+      font_style = "Arial"
+      font_size = 20
+      root = Tk()
+      w = root.winfo_screenwidth()
+      h = root.winfo_screenheight()
+      root.geometry("%dx%d+0+0"%(w,h))
+         
+      rfidString = StringVar()
+      rfidString.set('reset stuff')
 
-B = Tkinter.Button(top, text ="Hello", command = helloCallBack)
+      rfidtag="5465923269"
+      root.configure(background="#005c99")
+      Csong = Label(text="What's playing now:",bg = "#00e64d",font=(font_style, font_size),relief=RIDGE,width=20)
+      rfid = Label(text="Last Rfid Tap",relief=RIDGE,font=(font_style, font_size),width=20)
+      up_next = Label(text="Up next",relief=RIDGE,font=(font_style, font_size),width=20)
+      Csongtext = Label(text="Current Song",relief=RIDGE,font=(font_style, font_size),width=30)
+      rfidtext = Label(textvariable = rfidString,relief=RIDGE,font=(font_style, font_size),width=30)
+      up_nexttext = Label(text="Up Next Song",relief=RIDGE,font=(font_style, font_size),width=30)
+       
 
-B.pack()
-top.mainloop()
+      Csong.grid(row=1,column=1, padx =10, pady = 27)
+      rfid.grid(row=2,column=1)
+      up_next.grid(row=3,column=1, pady = 27)
+      Csongtext.grid(row=1,column=4,columnspan=2, padx=10)
+      rfidtext.grid(row=2,column=4,columnspan=2)
+      up_nexttext.grid(row=3,column=4,columnspan=2)
+
+
+
+
+      reset2 = Button(root, text ="Reset",bg = "red",font=(font_style, font_size), command = self.reset(self))
+      pause2 = Button(root, text ="Pause",relief=GROOVE,bg = "#00e64d", font=(font_style, font_size),
+                      command = self.pausePlay(self))
+      play2 = Button(root, text ="Play",font=(font_style, font_size), command = self.pausePlay(self))
+      skip2 = Button(root, text ="Skip", font=(font_style, font_size), command = self.skip(self))
+      skip2.grid(row=4,column=1,ipadx=20, pady=30)
+      pause2.grid(row=4,column=3,ipadx=14,padx=10)
+      reset2.grid(row=4,column=4,ipadx=20,padx=10)
+
+
+
+      root.mainloop()
--- Pi/runme.py
+++ Pi/runme.py
@@ -1,293 +1,307 @@
 #!/usr/bin/python
 
 __author__ = '100520993'
+from User import User
+from GUI import GUI
 import serial
 import pygame
 
-pygame
-pygame.init()
-pygame.mixer.init()
+class runme(object):
+    
 
-from User import User
+    pygame
+    pygame.init()
+    pygame.mixer.init()
+    
 
-finalSongList = []
-songsPlayed = []
-userList = []
-userRemovedList = []
-songIndex = 0
-vipUser = User([00,00,00,00,00])
-blankUser = User([0,0,0,0,0])
-try: 
-    ser = serial.Serial('/dev/ttyACM1', 9600)
-except Exception:
-    ser = serial.Serial('/dev/ttyACM0', 9600)
-pauseStatus = 0
-
-# checks to see if a tag is tapped on the rfid reader
-
-
-"""Adds or removes a user by rfid whether it has a text file or not
-@param rfidTag the list that contains the RFID tag number
-"""
-
-def reset():
-    global userList
-    global songIndex
-    global vipUser
-    global blankUser
-    global finalSongList
-    global songsPlayed
-    userList = []
+    finalSongList = []
     songsPlayed = []
-    finalSongList = []
-    createPlaylist()
+    userList = []
+    userRemovedList = []
     songIndex = 0
-    vipUser = blankUser
-    
-def checkVip(user):
-    global  vipUser
-    if vipUser.equals(user):
-        return True
-    return False
-def removeVip():
-    global vipUser
-    global blankUser
-    vipUser = blankUser
-    
-    
-"""Adds or removes a user by rfid whether it has a text file or not
-@param rfidTag the list that contains the RFID tag number
-"""
-def updateUser(rfidTag):
-    global vipUser
-    global blankUser
-    #boolean variables to see if the tag is in one of the lists already
-    inUserList = False
-    inUserRemovedList = False
-    user = User(rfidTag)
+    vipUser = User([00,00,00,00,00])
+    blankUser = User([0,0,0,0,0])
 
     
-    if vipUser.equals(blankUser):
-        vipUser = user
-        
-    #need to find the same user object reference whether it be in the userlist or in the removed user list
-    equalsUser = user
 
-    #checks if the user is in the userList
-    for u in userList:
-        inUserList = u.equals(user)
-        # found the user that is already in the database that is currently using its songs and tapped off
-        if inUserList:
-            equalsUser = u
-            break
-    # if its not in the user list, then we should check if its in the other removed list, since all of their songs
-    # might have been played
-    if not inUserList:
-        for u in userRemovedList:
-            inUserRemovedList = u.equals(user)
-            #found the user! now lets remove him from the system since he tapped off
-            if inUserRemovedList:
-                equalsUser = u # found the user that is already in the database and tapped off
-                break
+    try: 
+        ser = serial.Serial('/dev/ttyACM1', 9600)
+    except Exception:
+        ser = serial.Serial('/dev/ttyACM0', 9600)
+    pauseStatus = 0
+
+    # checks to see if a tag is tapped on the rfid reader
 
 
-
-    #the user tapped on to enter the system
-    if inUserList == False and not inUserRemovedList :
-        userList.append(user)
-        createPlaylist()
-    #if the user tapped off and all the songs in his playlist were played
-    elif inUserRemovedList:
-        userRemovedList.remove(equalsUser)
-        createPlaylist()
-        if checkVip(equalsUser):
-            removeVip()
-    #this is when the user has tapped off of the system but he is not out of songs to play
-    else:
-        userList.remove(equalsUser)
-        if checkVip(equalsUser):
-            removeVip()
-        createPlaylist()
-
-
-"""Gets the song list for the users currently in the system"""
-def getSongList():
-    global userList
-    songlist = []
-   
-    
-    # Searches through all user
-    for n in userList:
-        # Gets songs from users
-        n.resetSongIndex()
-        nextsong = n.getNextSong()
-        while nextsong != "OUTOFBOUNDS" and nextsong != "":
-            songlist.append(nextsong)
-            #print nextsong
-            nextsong = n.getNextSong()
-    # returns a list of songs
-    print "SongList:"
-    print songlist
-    return songlist
-
-
-"""Creates a playlist using the songs in the finalSongList"""
-def createPlaylist():
-    global finalSongList
-    global userList
-    global vipUser
-    
-    
-    
-    
-    print "VIP USER: "
-    print vipUser.getID()
-    finalSongList = []
-    songList = getSongList()
-    if songList:
-        uniqueSong = []
-        groupList = []
-        for n in range(len(userList)):
-            groupList.append([])
-            uniqueSong.append(songList[0])
-        for n in songList:
-            c = True
-            for j in uniqueSong:
-                if(n == j):
-                    c = False
-                    break
-            if c:
-                uniqueSong.append(n)
-        """
-        print uniqueSong
-        """
-        for n in uniqueSong:
-            count = -1
-            for j in songList:
-                if n == j:
-                    count += 1
-            if count >= len(userList):
-                count = len(userList)-1
-            if count >= 0:
-                groupList[count].append(n)
-        #here is where you randomize groupList[count]
-            
-        
-           
-        for n in range(len(groupList)-1,-1, -1):
-            for j in range(len(groupList[n])):
-                finalSongList.append(groupList[n][j])
-        """        
-        print "finalSongList"
-        print finalSongList
-        """
-    else:
-        finalSongList.append('')
-
-"""Gets the next song to be played"""
-def getNextSong():
-    global finalSongList
-    global songIndex
-    global songsPlayed
-    while(hasnotbeenPlayed(finalSongList[songIndex])):
-        songIndex += 1
-        if(songIndex >= (len(finalSongList))):
-            songIndex = 0
-            songsPlayed = []
-    songsPlayed.append(finalSongList[songIndex])
+    """Adds or removes a user by rfid whether it has a text file or not
+    @param rfidTag the list that contains the RFID tag number
     """
-    print "Songs Played"
-    print songsPlayed
-    print "Song in get nextSong"
-    print finalSongList[songIndex]
-    """
-    return finalSongList[songIndex]
 
-"""Checks if the song has alreay been played"""
-def hasnotbeenPlayed(song):
-    global songsPlayed
-    if not songsPlayed:
-        songsPlayed.append(song)
-        return False
-    elif (song == "OUTOFBOUNDS" or song == ''):
+    def reset(self):
+        global userList
+        global songIndex
+        global vipUser
+        global blankUser
+        global finalSongList
+        global songsPlayed
+
+        userList = []
+        songsPlayed = []
+        finalSongList = []
+        createPlaylist()
+        songIndex = 0
+        vipUser = blankUser
+        
+    def checkVip(user):
+        global  vipUser
+        if vipUser.equals(user):
             return True
-    else:
-        for n in songsPlayed:
-            if(n == song):
-                return True
         return False
-
-"""Pauses the music currently being played"""
-def pause():
-    global pauseStatus
-    global userList
-    if pauseStatus == 0 and not len(userList) == 0:
-        pygame.mixer.music.pause()
-        pauseStatus = 1
-    elif len(finalSongList) > 0:
+    def removeVip():
+        global vipUser
+        global blankUser
+        vipUser = blankUser
         
-        pygame.mixer.music.unpause()
-        pauseStatus = 0
         
+    """Adds or removes a user by rfid whether it has a text file or not
+    @param rfidTag the list that contains the RFID tag number
+    """
+    def updateUser(rfidTag):
+        global vipUser
+        global blankUser
+        #boolean variables to see if the tag is in one of the lists already
+        inUserList = False
+        inUserRemovedList = False
+        user = User(rfidTag)
 
-
-def skip():
-    global pauseStatus
-    if len(userList) >= 1:
-        pauseStatus = 0
-        skipSongName = getNextSong()
-        pygame.mixer.music.load(skipSongName)
-        print "Playing: " + skipSongName
-        ser.write(skipSongName)
-        ser.write('(')
-        pygame.mixer.music.play()
-    
-
-"""This is the input stream from the arduino to the Pi"""
-resetVar = False
-while 1:
-    x = ""
-    
-    if ser.inWaiting() > 0:
-        x = ser.readline().rstrip()
-        print "Arduino input:" + x
-
-    lst = []
-    if x == "RESET":
-        resetVar = True;
-        ser.write("Tap VIP")
-        ser.write(')')
         
-    elif x == "RFID":
-       
-        for i in range(0, 5):
-            lst.append(ser.readline().rstrip())
-        if resetVar == False:
-           
-            updateUser(lst)
-        else:
-            if(checkVip(User(lst))):
-                reset()
-            else:
-                
-                ser.write("INVALID VIP")
-                ser.write(')')
-                print("INVALID VIP")
-            resetVar = False
-
-    elif x == "PAUSE":
-        pause()
-
-    elif x == "SKIP":
-        skip()
-           
-    if len(userList) > 0 and not pygame.mixer.music.get_busy()  :
-        song = getNextSong()
-        #print song
-        if(song != ''):
-            pygame.mixer.music.load(song)
+        if vipUser.equals(blankUser):
+            vipUser = user
             
-            ser.write(song)
+        #need to find the same user object reference whether it be in the userlist or in the removed user list
+        equalsUser = user
+
+        #checks if the user is in the userList
+        for u in userList:
+            inUserList = u.equals(user)
+            # found the user that is already in the database that is currently using its songs and tapped off
+            if inUserList:
+                equalsUser = u
+                break
+        # if its not in the user list, then we should check if its in the other removed list, since all of their songs
+        # might have been played
+        if not inUserList:
+            for u in userRemovedList:
+                inUserRemovedList = u.equals(user)
+                #found the user! now lets remove him from the system since he tapped off
+                if inUserRemovedList:
+                    equalsUser = u # found the user that is already in the database and tapped off
+                    break
+
+
+
+        #the user tapped on to enter the system
+        if inUserList == False and not inUserRemovedList :
+            userList.append(user)
+            createPlaylist()
+        #if the user tapped off and all the songs in his playlist were played
+        elif inUserRemovedList:
+            userRemovedList.remove(equalsUser)
+            createPlaylist()
+            if checkVip(equalsUser):
+                removeVip()
+        #this is when the user has tapped off of the system but he is not out of songs to play
+        else:
+            userList.remove(equalsUser)
+            if checkVip(equalsUser):
+                removeVip()
+            createPlaylist()
+
+
+    """Gets the song list for the users currently in the system"""
+    def getSongList():
+        global userList
+        songlist = []
+       
+        
+        # Searches through all user
+        for n in userList:
+            # Gets songs from users
+            n.resetSongIndex()
+            nextsong = n.getNextSong()
+            while nextsong != "OUTOFBOUNDS" and nextsong != "":
+                songlist.append(nextsong)
+                #print nextsong
+                nextsong = n.getNextSong()
+        # returns a list of songs
+        print "SongList:"
+        print songlist
+        return songlist
+
+
+    """Creates a playlist using the songs in the finalSongList"""
+    def createPlaylist():
+        global finalSongList
+        global userList
+        global vipUser
+        
+        
+        
+        
+        print "VIP USER: "
+        print vipUser.getID()
+        finalSongList = []
+        songList = getSongList()
+        if songList:
+            uniqueSong = []
+            groupList = []
+            for n in range(len(userList)):
+                groupList.append([])
+                uniqueSong.append(songList[0])
+            for n in songList:
+                c = True
+                for j in uniqueSong:
+                    if(n == j):
+                        c = False
+                        break
+                if c:
+                    uniqueSong.append(n)
+            """
+            print uniqueSong
+            """
+            for n in uniqueSong:
+                count = -1
+                for j in songList:
+                    if n == j:
+                        count += 1
+                if count >= len(userList):
+                    count = len(userList)-1
+                if count >= 0:
+                    groupList[count].append(n)
+            #here is where you randomize groupList[count]
+                
+            
+               
+            for n in range(len(groupList)-1,-1, -1):
+                for j in range(len(groupList[n])):
+                    finalSongList.append(groupList[n][j])
+            """        
+            print "finalSongList"
+            print finalSongList
+            """
+        else:
+            finalSongList.append('')
+
+    """Gets the next song to be played"""
+    def getNextSong():
+        global finalSongList
+        global songIndex
+        global songsPlayed
+        while(hasnotbeenPlayed(finalSongList[songIndex])):
+            songIndex += 1
+            if(songIndex >= (len(finalSongList))):
+                songIndex = 0
+                songsPlayed = []
+        songsPlayed.append(finalSongList[songIndex])
+        """
+        print "Songs Played"
+        print songsPlayed
+        print "Song in get nextSong"
+        print finalSongList[songIndex]
+        """
+        return finalSongList[songIndex]
+
+    """Checks if the song has alreay been played"""
+    def hasnotbeenPlayed(song):
+        global songsPlayed
+        if not songsPlayed:
+            songsPlayed.append(song)
+            return False
+        elif (song == "OUTOFBOUNDS" or song == ''):
+                return True
+        else:
+            for n in songsPlayed:
+                if(n == song):
+                    return True
+            return False
+
+    """Pauses the music currently being played"""
+    def pausePlay():
+        global pauseStatus
+        global userList
+        if pauseStatus == 0 and not len(userList) == 0:
+            pygame.mixer.music.pause()
+            pauseStatus = 1
+        elif len(finalSongList) > 0:
+            
+            pygame.mixer.music.unpause()
+            pauseStatus = 0
+            
+
+
+    def skip():
+        global pauseStatus
+        if len(userList) >= 1:
+            pauseStatus = 0
+            skipSongName = getNextSong()
+            pygame.mixer.music.load(skipSongName)
+            print "Playing: " + skipSongName
+            ser.write(skipSongName)
             ser.write('(')
-            print "Playing: " + song
             pygame.mixer.music.play()
+            gui.updateSkip('lets go to the next song')
+        
+
+    """This is the input stream from the arduino to the Pi"""
+    resetVar = False
+
+    #create the GUI
+    gui = GUI(reset,pausePlay,skip)
+    while 1:
+        x = ""
+        
+        if ser.inWaiting() > 0:
+            x = ser.readline().rstrip()
+            print "Arduino input:" + x
+
+        lst = []
+        if gui.reset == "RESET":
+            print('yolo i got the reset stuff')
+            resetVar = True;
+            ser.write("Tap VIP")
+            ser.write(')')
+            
+        elif x == "RFID":
+           
+            for i in range(0, 5):
+                lst.append(ser.readline().rstrip())
+            if resetVar == False:
+               
+                updateUser(lst)
+            else:
+                if(checkVip(User(lst))):
+                    reset()
+                else:
+                    
+                    ser.write("INVALID VIP")
+                    ser.write(')')
+                    print("INVALID VIP")
+                resetVar = False
+
+        elif x == "PAUSE":
+            pausePlay()
+
+        elif x == "SKIP":
+            skip()
+               
+        if len(userList) > 0 and not pygame.mixer.music.get_busy()  :
+            song = getNextSong()
+            #print song
+            if(song != ''):
+                pygame.mixer.music.load(song)
+                
+                ser.write(song)
+                ser.write('(')
+                print "Playing: " + song
+                pygame.mixer.music.play()
+
