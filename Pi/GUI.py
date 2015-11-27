4fe110f7cb5aeca0df672f95669df602d3d7fb32
Bizarro <albert.fung@uoit.net>
2015-11-27 14:08:50 -0500

    LOLOL

--- Pi/GUI.py
+++ Pi/GUI.py
@@ -46,8 +46,8 @@ class GUI (object):
         self.root = Tk()
         #w = self.root.winfo_screenwidth()
         #h = self.root.winfo_screenheight()
-        w = 200
-        h = 100
+        w = 900
+        h = 400
         self.root.geometry("%dx%d+0+0"%(w,h))
 
 
