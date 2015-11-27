0d5c6ab1cbeb8870ec36a2e8337dc2f7cf97c45f
Bizarro <albert.fung@uoit.net>
2015-11-27 13:34:46 -0500

    LOLOL

--- Pi/GUI.py
+++ Pi/GUI.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import sys
+import subprocess
+
 from Tkinter import *
 
 class GUI (object):
@@ -8,8 +10,9 @@ class GUI (object):
     
     def quit(self): 
         print 'inside quit'
-        self.root.destroy()
-        sys.exit()
+        subprocess.call("lol.sh")
+        #self.root.destroy()
+        #sys.exit()
         #root.destroy()
 
     def updateSong(self,song):
@@ -73,7 +76,7 @@ class GUI (object):
         self.skip2 = Button(self.root, text ="Skip", font=(font_style, font_size), command = s)
 
         #self.kill = Button(self.root, text = "BYE", bg = "orange", font = (font_style, font_size), command = self.root.destroy)
-        self.kill = Button(self.root, text = "BYE", bg = "orange", font = (font_style, font_size), command = q)
+        self.kill = Button(self.root, text = "BYE", bg = "orange", font = (font_style, font_size), command = self.quit)
         self.skip2.grid(row=4,column=1,ipadx=20, pady=30)
         self.pause2.grid(row=4,column=3,ipadx=14,padx=10)
         self.reset2.grid(row=4,column=4,ipadx=20,padx=10)
