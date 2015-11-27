b96c0a36b4b58d534a7832ac75583451be45ad02
Bizarro <albert.fung@uoit.net>
2015-11-27 11:17:31 -0500

    Added root shit

--- Pi/runme.py
+++ Pi/runme.py
@@ -313,5 +313,5 @@ def main(threadName):
 #create the GUI
 gui = GUI(resetCallback,pausePlay,skip)
 thread.start_new(main,('main thread',))
-gui.protocol('WM_DELETE_WINDOW', quit)
+gui.getRoot().protocol('WM_DELETE_WINDOW', quit)
 gui.getRoot().mainloop()
