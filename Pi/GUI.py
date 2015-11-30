271590e100969b4643fa22f50f3d56d197383d9b
Janahan Mathanamohan <100523201@oncampus.local>
2015-11-30 18:43:54 -0500

    Added Comments

--- Pi/GUI.py
+++ Pi/GUI.py
@@ -8,6 +8,9 @@ from Tkinter import *
 class GUI (object):
     
     
+    """ Calls the quit script
+    @param calls itself
+    """
     def quit(self): 
         print 'inside quit'
         subprocess.call('./quit.sh')
@@ -15,23 +18,35 @@ class GUI (object):
         #sys.exit()
         #root.destroy()
 
+    """ Updates the text of the song
+    @param calls itself as well as the name of the current song playing
+    """
     def updateSong(self,song):
         self.Csongtext["text"] = song
 
+    """ Updates the text of the next song
+    @param calls itself as well as the name of the next song playing
+    """
     def updateNextSong(self,song):
         self.up_nexttext["text"] = song
 
+    """ Updates the text of the song
+    @param calls itself as well as the name of the current song playing
+    """
     def updateRFID(self,tag):
         print tag
         self.rfidtext["text"] = tag
         
-
+    """ Switches the pause to play and play to pause when button is pressed
+    @param calls itselfs and the current status
+    """    
     def updatePauseButton(self,status):
         self.pause2["text"] = status
         self.pause2.config(width = 7)
 
 
     def __init__(self,reset,pausePlay,skip, quit):
+        # methods to play skip quir and reset
         def r ():
             reset()
         def p ():
@@ -41,17 +56,19 @@ class GUI (object):
         def q ():
             quit()
 
+         # sets the font
         font_style = "Arial"
         font_size = 20
+        # intialize our gui
         self.root = Tk()
-        #w = self.root.winfo_screenwidth()
-        #h = self.root.winfo_screenheight()
+
+        # sets the width and height
         w = 850
         h = 350
         self.root.geometry("%dx%d+0+0"%(w,h))
 
 
-
+        # makes the background and the textboxes
         rfidtag="5465923269"
         self.root.configure(background="#005c99")
         self.Csong = Label(text="What's playing now:",font=(font_style, font_size),relief=RIDGE,width=20)
@@ -61,7 +78,7 @@ class GUI (object):
         self.rfidtext = Label(text = "",relief=RIDGE,font=(font_style, font_size),width=30)
         self.up_nexttext = Label(text="",relief=RIDGE,font=(font_style, font_size),width=30)
 
-
+        # places the textboxs on the gui
         self.Csong.grid(row=1,column=1, padx =10, pady = 27)
         self.rfid.grid(row=2,column=1)
         self.up_next.grid(row=3,column=1, pady = 27)
@@ -71,19 +88,22 @@ class GUI (object):
 
 
 
-
+        # makes the buttons
         self.reset2 = Button(self.root, text ="Reset",bg = "red",font=(font_style, font_size), command = r)
         self.pause2 = Button(self.root, text ="Pause",relief=GROOVE,bg = "#00e64d", font=(font_style, font_size), command = p)
         self.pause2.config(width = 7)
         self.skip2 = Button(self.root, text ="Skip",bg = "orange", font=(font_style, font_size), command = s)
 
-        #self.kill = Button(self.root, text = "BYE", bg = "orange", font = (font_style, font_size), command = self.root.destroy)
-        #self.kill = Button(self.root, text = "BYE", bg = "orange", font = (font_style, font_size), command = self.quit)
+        # places the buttons on the gui        
         self.skip2.grid(row=4,column=1,ipadx=20, pady=30)
         self.pause2.grid(row=4,column=2,ipadx=14,padx=10)
         self.reset2.grid(row=4,column=3,ipadx=20,padx=10)
         #self.kill.grid(row=4,column =5, ipadx = 10, padx=10)
 
 
+    """ Returns the root
+    @param  calls itself
+    @return the gui value
+    """    
     def getRoot(self):
         return self.root
--- Pi/runme.py
+++ Pi/runme.py
@@ -8,7 +8,7 @@ import pygame
 import thread
 import sys
 import subprocess
-
+# Variables 
 finalSongList = []
 songsPlayed = []
 userList = []
@@ -24,21 +24,26 @@ global skipBool
 global initialized
 initialized = False
 
+# Try catch loop for serial 
 try:
     ser = serial.Serial('/dev/ttyACM1', 9600)
 except Exception:
     ser = serial.Serial('/dev/ttyACM0', 9600)
 pauseStatus = 0
 
-# checks to see if a tag is tapped on the rfid reader
-
+"""Calls the script to exit on clicking the windows close button
+"""
 def quit():
     print 'inside quit 2'
     subprocess.call('./quit.sh')
 
+"""Call back for quit
+"""
 def quit_callback():
     gui.quit()
 
+"""Call back for reset
+"""
 def resetCallback():
     gui.updateRFID('Tap VIP User')
     global resetVar
@@ -47,6 +52,8 @@ def resetCallback():
 @param rfidTag the list that contains the RFID tag number
 """
 
+"""Resets all the variables when reset is pressed
+"""
 def reset():
     print 'reset called'
     global userList
@@ -69,11 +76,20 @@ def reset():
     gui.updateNextSong(nextSong)
     firstSongLoaded = False
 
+
+"""Checks for vip user 
+@param user is an user id
+@return boolean
+"""
 def checkVip(user):
+    # Gets global variables
     global  vipUser
+    # Checks if the user is a vip user
     if vipUser.equals(user):
         return True
     return False
+"""Sets the vip user to an empty user 
+"""
 def removeVip():
     global vipUser
     global blankUser
@@ -172,12 +188,16 @@ def createPlaylist():
     print vipUser.getID()
     finalSongList = []
     songList = getSongList()
+    # Checks uf the song list is not emo=pty
     if songList:
+        # resets the lists to empty array
         uniqueSong = []
         groupList = []
+        # gets all the songs for user list
         for n in range(len(userList)):
             groupList.append([])
             uniqueSong.append(songList[0])
+        # gets all the songs for unique songs
         for n in songList:
             c = True
             for j in uniqueSong:
@@ -186,7 +206,7 @@ def createPlaylist():
                     break
             if c:
                 uniqueSong.append(n)
-
+        # for each song in the list we add a counter for the same values
         for n in uniqueSong:
             count = -1
             for j in songList:
@@ -196,10 +216,8 @@ def createPlaylist():
                 count = len(userList)-1
             if count >= 0:
                 groupList[count].append(n)
-        #here is where you randomize groupList[count]
 
-
-
+        # adds the songs to the current list that is 
         for n in range(len(groupList)-1,-1, -1):
             for j in range(len(groupList[n])):
                 finalSongList.append(groupList[n][j])
@@ -210,24 +228,33 @@ def createPlaylist():
 
 
 
-"""Gets the next song to be played"""
+"""Gets the next song to be played
+@return nest song
+"""
 def getNextSong():
     global finalSongList
     global songIndex
     global songsPlayed
+    # Checks if final song list is going to go out of bounds
     if(songIndex >= len(finalSongList)):
+        # resets value to 0
         songIndex = 0
+    # Gets the next song to be played
     while(hasnotbeenPlayed(finalSongList[songIndex])):
         songIndex += 1
         if(songIndex >= (len(finalSongList))):
             songIndex = 0
             songsPlayed = []
+    # adds to songs played
     songsPlayed.append(finalSongList[songIndex])
     return finalSongList[songIndex]
 
-"""Checks if the song has alreay been played"""
+"""Checks if the song has alreay been played
+@param the song
+@return a boolean value if the song is played or not"""
 def hasnotbeenPlayed(song):
     global songsPlayed
+    # checks if the songs are played
     if not songsPlayed:
         songsPlayed.append(song)
         return False
@@ -242,12 +269,14 @@ def hasnotbeenPlayed(song):
 """Pauses the music currently being played"""
 def pausePlay():
     print 'pause play called'
-    
+    # gets the global variables
     global pauseStatus
     global userList
     global gui
     global pygame
+    # check if paused
     if pauseStatus == 0:
+        # calls pause from py game
         if pygame.mixer.music.get_busy():
             pygame.mixer.music.pause()
             pauseStatus = 1
@@ -258,8 +287,10 @@ def pausePlay():
         pauseStatus = 0
 
 
-
+"""Skip the current song being played
+"""
 def skip():
+    # calling global variables
     global pauseStatus
     global firstSongLoaded
     global currentSong
@@ -268,15 +299,21 @@ def skip():
     global initialized
     global gui
     skipBool = True
+    # checks if the intialized is set to true
     if initialized:
+        # Checks if the length of the user list is greater than 1
         if len(userList) >= 1:
+            # Checks if the first song was not loaded
             if( not firstSongLoaded):
+                # gets the next song of the list sets it to trye
                 currentSong = getNextSong()
                 firstSongLoaded = True
             else:
+                # gets the song next song 
                 currentSong = nextSong
+            # gets the next song and stores it
             nextSong = getNextSong()
-
+            # does all the calls for playing the next song
             pygame.mixer.music.load(currentSong)
             gui.updateSong(currentSong)
             gui.updateNextSong(nextSong)
@@ -284,15 +321,20 @@ def skip():
     skipBool = False
     print 'skip called'
 
+
 """This is the input stream from the arduino to the Pi"""
 resetVar = False
 
 
-
+"""
+Main program that loops 
+@param theardName a string for the main programs thread
+"""
 def main(threadName):
+    # get global variables
     global gui
     global resetVar
-
+    # starts our music playing library pygame
     pygame
     pygame.init()
     pygame.mixer.init()
@@ -308,46 +350,53 @@ def main(threadName):
     print 'main thread called'
     counter = 0;
     initialized = True
+
+    # infinite loop to continously check for actions from the gui and arduino controller
     while 1:
 
         x = ""
-        
-        if counter %100000 == 1 :
-            print 'loop'
-        counter = counter + 1
+        # Checks for an input from serial
         if ser.inWaiting() > 0:
+            # Gets the input from serial
             x = ser.readline().rstrip()
             print "Arduino input:" + x
 
+        # resests the lst to emptu
         lst = []
-
+        # Checks if x has an RFID value
         if x == "RFID":
             
+            # spits the RFID tage value to an array of 5
             for i in range(0, 5):
                 lst.append(ser.readline().rstrip())
-            
+            # Checks if reset button was pressed
             if resetVar == False:
+                # update user list 
                 updateUser(lst)
+                # Checks if the length of user if equal to 0
                 if(len(userList) == 0):
+                    # sends updates to the qui
                     print 'updating song'
                     nextSong = ''
                     gui.updateNextSong(nextSong)
                     firstSongLoaded = False
             else:
+                # check if a vip user was pressed
                 if(checkVip(User(lst))):
                     reset()
                 else:
-
+                    # invalid vip user
                     gui.updateRFID('INVALID VIP')
                     print("INVALID VIP")
                 resetVar = False
 
 
-
+        # Checks if their are users in a list and if the skip
         if len(userList) > 0 and not pygame.mixer.music.get_busy() and skipBool == False:
             skip()
             print 'in skip loop'
             skipBool = False
+
 
 
 
